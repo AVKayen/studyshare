888c19c666cfba91828bd2242376d316ac157780
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2024-12-21 17:16:36 +0100

    Create task upload route.

--- src/main/kotlin/com/physman/routes/Task.kt
+++ src/main/kotlin/com/physman/routes/Task.kt
@@ -1,13 +1,39 @@
 package com.physman.routes
 
 import com.physman.task.InMemoryTaskRepository
+import com.physman.task.Task
 import com.physman.templates.index
 import io.ktor.http.*
 import io.ktor.server.html.*
 import io.ktor.server.routing.*
 import com.physman.templates.taskTemplate
+import io.ktor.server.request.*
+import kotlinx.html.body
+import kotlin.random.Random
 
 fun Route.taskRouter() {
+
+    post {
+        val formParameters = call.receiveParameters()
+        val title = formParameters["title"].toString()
+        val additionalNotes = formParameters["additionalNotes"].toString()
+
+        if(title.isEmpty() || title.length > 48) {
+            call.response.status(HttpStatusCode.BadRequest)
+            return@post
+        }
+
+        val newTask = Task(id = Random.nextInt(99999), title = title, additionalNotes = additionalNotes)
+
+        val task = InMemoryTaskRepository.createTask(newTask)
+
+        call.respondHtml(HttpStatusCode.OK) {
+            body {
+                taskTemplate(task)
+            }
+        }
+    }
+
     route("/{id}") {
         get {
             val taskId = call.parameters["id"]?.toIntOrNull()
--- src/main/kotlin/com/physman/templates/Task.kt
+++ src/main/kotlin/com/physman/templates/Task.kt
@@ -5,7 +5,7 @@ import kotlinx.html.FlowContent
 import kotlinx.html.*
 
 fun FlowContent.taskTemplate(task: Task) {
-    div {
+    div(classes = "flex-col") {
         div {
             h2 {
                 +task.title
@@ -13,6 +13,7 @@ fun FlowContent.taskTemplate(task: Task) {
         }
         div {
             if(task.additionalNotes != null) {
+                println(task.additionalNotes)
                 +"Notes: ${task.additionalNotes}"
             }
         }
