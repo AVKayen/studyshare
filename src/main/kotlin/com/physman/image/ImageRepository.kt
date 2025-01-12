f32c1e628d025b28a12e341da2bd47aebd44f500
AVKayen <aganitnt@gmail.com>
2025-01-12 12:05:08 +0100

    Revert "Add mimeType support for image repository (unused as of now)"
    This reverts commit 2a9147fc014b750403306525edbc469eb20c6526.

--- src/main/kotlin/com/physman/image/ImageRepository.kt
+++ src/main/kotlin/com/physman/image/ImageRepository.kt
@@ -1,7 +1,7 @@
 package com.physman.image
 
 interface ImageRepository {
-    suspend fun createImage(image: Image, content: ByteArray, mimeType: String = "application/octet-stream"): Image
+    suspend fun createImage(image: Image, content: ByteArray): Image
     suspend fun deleteImage(id: String): Unit
     suspend fun getImageLink(id: String): String?
 }
