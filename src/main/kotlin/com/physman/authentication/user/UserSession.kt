6afd521b7df0c27de0796b257ef93465c59a160f
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2025-01-16 18:10:35 +0100

    Remove unnecessary code from UserSession.

--- src/main/kotlin/com/physman/authentication/user/UserSession.kt
+++ src/main/kotlin/com/physman/authentication/user/UserSession.kt
@@ -1,16 +1,9 @@
 package com.physman.authentication.user
 
 import kotlinx.serialization.Serializable
-import org.bson.BsonType
-import org.bson.codecs.pojo.annotations.BsonId
-import org.bson.codecs.pojo.annotations.BsonRepresentation
-import org.bson.types.ObjectId
 
 @Serializable
 data class UserSession(
-    @BsonId
-    @BsonRepresentation(BsonType.OBJECT_ID)
-
-    val id: String = ObjectId().toHexString(),
+    val id: String,
     val name: String,
 )
