c7afd5afd307eb075784de98e6ffa1ec5ed0378b
AVKayen <aganitnt@gmail.com>
2025-01-18 15:07:11 +0100

    Specify deployment configuration for google app engine

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,13 @@
+import com.google.cloud.tools.gradle.appengine.appyaml.AppEngineAppYamlExtension
+
 val kotlin_version: String by project
 val logback_version: String by project
-
 plugins {
     kotlin("jvm") version "2.1.0"
     id("io.ktor.plugin") version "3.0.2"
     id("org.jetbrains.kotlin.plugin.serialization") version "2.1.0"
+    id("com.gradleup.shadow") version "8.3.1"
+    id("com.google.cloud.tools.appengine") version "2.8.0"
 }
 
 group = "com.physman"
@@ -12,9 +15,14 @@ version = "0.0.1"
 
 application {
     mainClass.set("io.ktor.server.netty.EngineMain")
-
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(20))
+    }
 }
 
 repositories {
@@ -36,9 +44,20 @@ dependencies {
     implementation("org.mongodb:bson-kotlinx:5.2.1")
     implementation("org.mongodb:mongodb-driver-kotlin-coroutine:5.2.1")
 
-    // implementation(platform("com.google.cloud:libraries-bom:26.51.0")) // ignore this
+    implementation(platform("com.google.cloud:libraries-bom:26.51.0")) // ignore this (or don't)
     implementation("com.google.cloud:google-cloud-storage:2.46.0")
+    implementation("com.google.cloud:google-cloud-datastore")
 
     testImplementation("io.ktor:ktor-server-test-host-jvm")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+}
+
+configure<AppEngineAppYamlExtension> {
+    stage {
+        setArtifact("build/libs/${project.name}-all.jar")
+    }
+    deploy {
+        version = "GCLOUD_CONFIG"
+        projectId = "GCLOUD_CONFIG"
+    }
 }
