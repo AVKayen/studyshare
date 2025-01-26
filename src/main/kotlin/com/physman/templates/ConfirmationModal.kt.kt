c5507a8dc91bbfd9037ae7820cc8a87e74cfad7f
Mateusz Wojewódzki <matwojewodzki@gmail.com>
2025-01-26 10:32:35 +0100

    Create confirmationModalTemplate.

---
+++ src/main/kotlin/com/physman/templates/ConfirmationModal.kt.kt
@@ -0,0 +1,74 @@
+package com.physman.templates
+
+import kotlinx.html.*
+
+fun FlowContent.confirmationModalTemplate(title: String, details: String, submitText: String, submitAttributes: Map<String, String>) {
+
+    val animationDuration = "300ms"
+    val openClass = ".modal-is-open"
+    val openingClass = ".modal-is-opening"
+    val closingClass = ".modal-is-closing"
+    val scrollbarWidthCssVar = "--pico-scrollbar-width"
+
+    val dialogScript = """
+        def getScrollbarWidth()
+            return window.innerWidth - document.documentElement.clientWidth
+        end
+         
+        on load 1
+            set scrollbarWidth to getScrollbarWidth()
+            if scrollbarWidth
+                call document.documentElement.style.setProperty('$scrollbarWidthCssVar', scrollbarWidth + 'px')
+            end
+            add $openClass $openingClass to body
+            add @open='true'
+            wait $animationDuration
+            remove $openingClass from body
+        end
+        
+        on closeModal
+            add $closingClass to body
+            wait $animationDuration
+            remove $closingClass $openClass from body
+            call document.documentElement.style.removeProperty('$scrollbarWidthCssVar')
+            remove me
+    """.trimIndent()
+
+    dialog {
+        attributes["_"] = dialogScript
+
+        div(classes = "modal-content") {
+            article {
+                header {
+                    button {
+                        attributes["type"] = "button"
+                        attributes["aria-label"] = "Close"
+                        attributes["rel"] = "prev"
+                        attributes["_"] = "on click trigger closeModal"
+                    }
+                    h2 {
+                        +title
+                    }
+
+                }
+
+                +details
+
+                footer {
+                    button(classes = "secondary") {
+                        attributes["role"] = "button"
+                        attributes["type"] = "button"
+                        attributes["_"] = "on click trigger closeModal"
+
+                        +"Cancel"
+                    }
+
+                    button {
+                        attributes.putAll(submitAttributes)
+                        +submitText
+                    }
+                }
+            }
+        }
+    }
+}
